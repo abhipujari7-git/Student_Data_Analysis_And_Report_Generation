2d6a1b57f5dca96d591cf3d4a34b0071b7247acc
Abhishek Pujari <130705174+abhipujari7-git@users.noreply.github.com>
2023-04-20 15:10:55 +0530

    Delete Faculty.java

--- Faculty.java
+++
@@ -1,9 +0,0 @@
-class Faculty extends User {
-  String subjects[];
-  String grade;
-  
-  public void addMarks(studentId, subjectId, assesmentId, marks);
-  public void addAttendance(studentId, isPresent, date);
-  public void addTimetable();
-  public void generateReports(studentId);
-}
