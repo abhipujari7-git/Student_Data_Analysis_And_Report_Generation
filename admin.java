7b2762604de155a4511535d0357a88c410f34ada
Abhishek Pujari <130705174+abhipujari7-git@users.noreply.github.com>
2023-04-20 15:13:10 +0530

    Delete admin.java

--- admin.java
+++
@@ -1,9 +0,0 @@
-class admin extends User {  
-  public void addCourse(id, details);
-  public String removeCourse(id);
-  public void addFaculty(id, name, details);
-  public void modifyFaculty(id, field ,newValue);
-  public void addStudent(id, name, details);
-  public void modifyStudent(id, field ,newValue);
-  public void manageAttendance();
-}
